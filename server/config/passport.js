da7bd13ffc9e280e852e44b81549a0deff9571c5
Lior-Yosef <lioryosef06@gmail.com>
2022-03-06 22:41:00 +0200

    ADD AUTO

---
+++ server/config/passport.js
@@ -0,0 +1,24 @@
+
+const jwt_Strategy = require('passport-jwt').Strategy;
+const jwt_ExtractJwt = require('passport-jwt').ExtractJwt;
+const users = require('../models/User_model');
+const SECRET_KEY = process.env.SECRET_KEY;
+
+const options = {
+    secretOrKey: SECRET_KEY
+}; 
+options.jwtFromRequest = jwt_ExtractJwt.fromAuthHeaderAsBearerToken(); 
+
+module.exports = (passport) => {
+    passport.use(
+        new jwt_Strategy(options, (jwt_payload, done) => {  
+            console.log(jwt_payload);
+            users.findById( jwt_payload.User._id)
+                .then(user => {
+                    if (user) return done(null, user); 
+                    done(null, false);
+                })
+                .catch(err => console.log(err));
+        })
+    )       
+}
--- server/server.js
+++ server/server.js
@@ -8,10 +8,14 @@ const app = express();
 
 app.use(cors());
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
+require('./config/passport')(passport);
+app.use(passport.initialize());
 
 
-app.use('/animal',AnimalRoutes);
-app.use('/employee', EmployeeRoutes);
+app.use('/animal', passport.authenticate('jwt', { session: false }),AnimalRoutes);
+app.use('/employee', passport.authenticate('jwt', { session: false }), EmployeeRoutes);
 
 
 app.listen(process.env.PORT)
